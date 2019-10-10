9e2b255ff32d0f8917c6e899195c3c052b027d98
ahmedbing <ahmedbingol@sabanciuniv.edu>
2019-10-10 08:17:37 +0300

    .

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,7 @@
-#!/usr/bin/python
 
+from sklearn import linear_model
+from sklearn import metrics
+import numpy as np
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -8,13 +10,25 @@ def outlierCleaner(predictions, ages, net_worths):
         and the actual net worth).
 
         Return a list of tuples named cleaned_data where 
-        each tuple is of the form (age, net_worth, error).
+        each tuple is of the form (age, net_worth,error).
     """
-    
-    cleaned_data = []
 
-    ### your code goes here
+    for x in range(10):
+        max =0
+        ind_del=0
+        for idx , num in enumerate(net_worths):
+            dif=num-predictions[idx]
+            dif=np.abs(dif)
+            if (dif>max):
+                max = dif
+                ind_del= idx
+        ages = np.delete(ages,ind_del)
+        net_worths = np.delete(net_worths, ind_del )
+        print("Index deleted ", ind_del,"  At iteration = " ,x)
+        predictions = np.delete(predictions,ind_del)
 
-    
+    error = np.zeros(90)
+    cleaned_data = ages, net_worths, error
+
     return cleaned_data
 
